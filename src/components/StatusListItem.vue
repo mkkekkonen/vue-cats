26f02e76ffe4873d6ad203a0ec2c794c2b99915f
Maija Kekkonen <mkkekkonen@protonmail.com>
2019-12-01 01:34:01 +0200

    Render status codes

---
+++ src/components/StatusListItem.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="flexbox">
+    <b>{{ status.code }}</b>
+    &nbsp;&mdash;&nbsp;
+    <span>{{ status.phrase }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'StatusListItem',
+  props: {
+    status: Object
+  }
+}
+</script>
+
+<style scoped>
+.flexbox {
+  display: flex;
+  font-size: 135%;
+  padding: 0.5rem;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,44 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="outer-container">
+      <div class="inner-container">
+        <StatusListItem
+          v-for="status in statusCodes"
+          v-bind:status="status"
+          v-bind:key="status.spec_title"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
+import StatusListItem from '@/components/StatusListItem.vue'
+import statusCodes from '../data/status-codes.json'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
-  }
+    // HelloWorld
+    StatusListItem
+  },
+  data: () => ({
+    statusCodes
+  })
 }
 </script>
+
+<style scoped>
+.outer-container {
+  display: flex;
+  justify-content: center;
+  padding-top: 40px;
+}
+
+.inner-container {
+  display: inline-block;
+}
+</style>
